563fcd3f87f57f6a0d1892c681c0ed574ff5a1c3
Joel Klabo <joelklabo@gmail.com>
2012-04-13 12:01:57 -0700

    Adding message when no files are found

--- find-then-vim.sh
+++ find-then-vim.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "searching for $1 and opening results in vim..."
 OUTPUT=$(find . -name $1) 
-mvim -p $OUTPUT
+if [ "${OUTPUT}" == "" ]; then
+  echo "No files were found matching: $1"
+else
+  mvim -p $OUTPUT
+fi
+
 
 
